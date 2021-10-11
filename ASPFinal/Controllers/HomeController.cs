49e61b0c9cbe3011f8dbb9a45864724f6118904f
Amine <amine.bn7@gmail.com>
2021-10-11 15:23:26 -0500

    Added contact validation and browse by author

--- ASPFinal/Controllers/HomeController.cs
+++ ASPFinal/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPFinal.Models;
+using ASPFinal.Models.EntityFramework;
 
 namespace ASPFinal.Controllers
 {
     public class HomeController : Controller
     {
+        private HenryBookstoreASPDatabase dbo = new HenryBookstoreASPDatabase();
+
         public ActionResult Index()
         {
             return View();
@@ -15,7 +19,20 @@ namespace ASPFinal.Controllers
 
         public ActionResult BrowseByAuthor()
         {
-            return View();
+            var allauthors = dbo.AUTHORs.ToList();
+            List<AUTHOR> result = new List<AUTHOR>();
+
+            foreach (var auth in allauthors)
+            {
+                var model = new AUTHOR();
+                model.AUTHOR_NUM = auth.AUTHOR_NUM;
+                model.AUTHOR_FIRST = auth.AUTHOR_FIRST;
+                model.WROTEs = dbo.WROTEs.Where(c => c.AUTHOR_NUM == auth.AUTHOR_NUM).ToList();
+                result.Add(model);
+            }
+
+            //data needed in the view
+            return View(result);
         }
 
         public ActionResult BrowseByLocation()
@@ -35,8 +52,12 @@ namespace ASPFinal.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Contact(Contact contactData)
+        {
             return View();
         }
     }
---
+++ ASPFinal/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPFinal.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
